5b94d67683265af93069f90b6c6a2e03216bd612
Ishaan <ishaan.mandla@gmail.com>
2025-11-18 18:52:06 +0530

    This is the commit

--- IIC/adminsite/urls.py
+++ IIC/adminsite/urls.py
@@ -7,9 +7,25 @@ from . import views
 
 urlpatterns = [
     path('', views.homepage , name = "admin-site"),
+    path('profile/', views.profilePage , name = "admin-profile"),
+
+    path('info-update' , views.iicInfoEdit , name = "info-update"),
+
     path('post-create/' , views.postCreate , name = "post-create"),
     path('post-update/<int:pk>' , views.postEdit , name = "post-update"),
     path('post-delete/<int:pk>' , views.postDeletion , name = "post-delete"),
+
+    path('meet-create/' , views.add_meetform , name = "meet-create"),
+    path('meet-update/<int:pk>' , views.update_meetform , name = "meet-update"),
+    path('meet-delete/<int:pk>' , views.delete_meetform , name = "meet-delete"),
+
+    path('gallery-create/' , views.galleryCreate , name = "gallery-create"),
+    path('gallery-delete/<int:pk>' , views.galleryDeletion , name = "gallery-delete"),
+
+    path('notice-create/' , views.noticeCreate , name = "notice-create"),
+    path('notice-update/<int:pk>' , views.noticeEdit , name = "notice-update"),
+    path('notice-delete/<int:pk>' , views.noticeDeletion , name = "notice-delete"),
+
 
     path('achievment-create/' , views.achievCreate , name = "achievment-create"),
     path('achievment-update/<int:pk>' , views.achievEdit , name = "achievment-update"),
